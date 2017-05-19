1b6483993638d7374ddd7873e4676e04c59c3a09
seattlefella <thughes@valnetventures.com>
2017-05-19 14:25:22 -0700

    Lesson-32  NamePlate billboard script added and build tested.

--- Assets/Scripts/PlayerNamePlate.cs
+++ Assets/Scripts/PlayerNamePlate.cs
@@ -10,11 +10,25 @@ namespace Assets.Scripts
         private Text userNameText;
 
         [SerializeField]
+        private RectTransform healthBarFill;
+
+        [SerializeField]
         private Player player;
+
+        [SerializeField]
+        private Camera m_Camera;
 
         void Update()
         {
+            m_Camera = Camera.main;
+
+            transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+
             userNameText.text = player.name;
+
+            healthBarFill.localScale = new Vector3(player.GetHealthPct(), 1,1);
         }
     }
 }
+
