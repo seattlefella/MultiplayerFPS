5efd9c7d301fd5b54a46551280261f8a62102be6
seattlefella <thughes@valnetventures.com>
2017-05-19 12:17:01 -0700

    Lesson-32 Added the script to control the NamePlate and show the user name.

---
+++ Assets/Scripts/PlayerNamePlate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class PlayerNamePlate : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Text userNameText;
+
+        [SerializeField]
+        private Player player;
+
+        void Update()
+        {
+            userNameText.text = player.name;
+        }
+    }
+}
